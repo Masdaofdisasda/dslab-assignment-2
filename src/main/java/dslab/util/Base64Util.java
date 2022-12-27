d71bce236f86bc59631a40f7b2107e4a332d7d71
Hong-Li W <hong-li.wang@student.tuwien.ac.at>
2022-12-28 00:51:35 +0100

    Add a Base64 helper util

---
+++ src/main/java/dslab/util/Base64Util.java
@@ -0,0 +1,23 @@
+package dslab.util;
+
+import java.util.Base64;
+
+public class Base64Util {
+
+    private final static Base64Util INSTANCE = new Base64Util();
+
+    private Base64Util() {
+    }
+
+    private static Base64Util getInstance() {
+        return INSTANCE;
+    }
+
+    public String encode(byte[] bytes) {
+        return Base64.getEncoder().encodeToString(bytes);
+    }
+
+    public byte[] decode(String base64) {
+        return Base64.getDecoder().decode(base64);
+    }
+}
